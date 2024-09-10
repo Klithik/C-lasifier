ddf0ce47f2ab93a841df3b6b5c7d4b248581d938
Klithik <santi17461@gmail.com>
2024-09-10 10:22:29 -0300

    deletion of the previous check function

--- main.c
+++ main.c
@@ -2,25 +2,13 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
-int verifyDir(char target[]){
-    struct stat s;
-    int err = stat(target,&s);
-    if(err == -1){
-        return 1;
-    }
-    if(!S_ISDIR(s.st_mode)){
-        return 1;
-    }
-    return 0;
-}
-
 int main(){
     printf("Welcome to C-lasifier, what directory would you like to get in order:\n");
     char *input;
     scanf("%s", input);
-    if(verifyDir(input)==1){return 1;}
-
     struct dirent *target;
+
+    // Verify valid directory
     DIR *dir = opendir(input);
     if(dir == NULL){return 1;}
 
